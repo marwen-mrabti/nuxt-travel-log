64e40dce0954a1838a7a0b58a6a803da1a9fe381
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-18 01:34:30 +0100

    ci: eslint / editor setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/eslint"],
+
+  css: ["~/assets/css/main.css"],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+});
