661b3db9606ababd13cf9fc5033a93d1238f28ef
marwen <marwen.mrabti.me@gmail.com>
2025-06-18 03:19:49 +0100

    ci: setup tailwindcss / daisyUI / icons (#52)

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-4xl font-extrabold text-green-900">
+      Welcome to Nuxt
+    </h1>
+    <button class="btn">
+      View Map  <Icon
+        name="tabler:map"
+        style="color: white"
+        size="32"
+      />
+    </button>
   </div>
 </template>
