eaad25cdc921cd4f4f52579d958f1d71500db2b6
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-18 14:56:15 +0100

    ci: setup typesafe env

---
+++ lib/try-parse-env.ts
@@ -0,0 +1,26 @@
+/* eslint-disable node/no-process-env */
+import type { ZodObject, ZodRawShape } from "zod/v4";
+
+import { ZodError } from "zod";
+
+export default function tryParseEnv<T extends ZodRawShape>(EnvSchema: ZodObject<T>, buildEnv: Record<string, string | undefined> = process.env) {
+  try {
+    EnvSchema.parse(buildEnv);
+  }
+  catch (error) {
+    if (error instanceof ZodError) {
+      console.log("Processing ZodError with issues:", error.issues); // Add this
+      let message = "Missing required values in .env:\n";
+      error.issues.forEach((issue) => {
+        message += `${issue.path[0]}\n`;
+      });
+      const e = new Error(message);
+      e.stack = "";
+      console.log("Throwing custom error:", message); // Add this
+      throw e;
+    }
+    else {
+      console.error(error);
+    }
+  }
+}
